30a5db0afdd9e8e82a9cfcd60395062e9101c753
bri <bcamesi@gmail.com>
2022-11-13 18:52:40 -0500

    Clean up coding/css

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 // Import Dependencies
 // -----------------------------------------------
 
-require("dotenv").config() // load variables from .env into process.env
-const express = require('express'); // backend framework
+require("dotenv").config() 
+const express = require('express'); 
 const budgets = require("./models/budget")
 const morgan = require("morgan")
-const methodOverride = require("method-override") // import method-override
+const methodOverride = require("method-override")
 
 
 // -----------------------------------------------
@@ -33,18 +33,6 @@ app.use('/public', express.static('public'));
 // Make /budgets the index page
 app.get("/", (req, res) => res.redirect("/budgets"))
 
-// app.get("/budgets/", (req, res) => {
-//     res.render("index.ejs")
-// })
-
-// app.get("/budgets/:name", (req, res) => {
-//     res.render("show.ejs")
-// })
-
-// app.get("/budgets/new", (req, res) => {
-//     res.render("new.ejs")
-// })
-
 // Return all budget data
 
 app.get('/budgets/', (req, res) => {
@@ -55,15 +43,6 @@ app.get('/budgets/', (req, res) => {
         }
     );
 });
-
-// app.get('/budgets/:index', (req, res) => {
-//     res.render(
-//         'show.ejs',
-//         {
-//             budgets:budgets
-//         }
-//     );
-// });
 
 // -----------------------------------------------
 // New Route
