fddd1ce4dbee30f51712d17231ac841d769dc10f
bri <bcamesi@gmail.com>
2022-11-15 14:08:25 -0500

    Add partials and bg color change

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // -----------------------------------------------
 // Import Dependencies
 // -----------------------------------------------
@@ -82,3 +83,4 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
